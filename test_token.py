443c3d29bd996d94a69ca1a0f687ea7ea7124869
Evan Bourgoine <72997006+evanbourgoine@users.noreply.github.com>
2026-02-26 17:24:24 -0500

    whoops
    Replaced sensitive information with placeholders for app_id, app_secret, and short_token.

--- test_token.py
+++ test_token.py
@@ -2,9 +2,9 @@ import requests
 from dotenv import load_dotenv
 load_dotenv()
 
-app_id = '1202883444954046'
-app_secret = '9c51652f08d993d3f34e5704897a7432'
-short_token = 'EAARGBCBxU74BQ9HIuTZCr0B2LmluAw92yhD3tAz2i3o6si2RA1IO9AFqCyf3ZCKoQjZCCtgEN5esk30wMxpjUeZBS2eczm4CG1rWHxBtxS1SKkdwukU40jmeQbcLZBEYdZAsoKg1CJTynVgNpNrjB5amOkZBYyh3nRMI46eRuaxzIy4Vd8XAsJ0bpiEhZBGQQIBYwl6TaZBsnKkmIvr6ZANDPlvFnzoDBtbf8x1zwa2AFZArnMPBonErANPxXS2TM9weVyfIjiCbmqMBCFTtgHS5Lfsud13TgZDZD'
+app_id = '....'
+app_secret = '....'
+short_token = '....'
 
 response = requests.get(
     'https://graph.facebook.com/oauth/access_token',
